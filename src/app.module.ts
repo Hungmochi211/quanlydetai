41757bb5d58cb9d519c135df35bac7b0034d75ce
Hungmochi211 <Hungdztv165@gmail.com>
2026-07-15 14:34:27 +0700

    Thêm env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { SpecModule } from './spec/spec.module';
 import { ProjectModule } from './project/project.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NguoiDung } from 'src/entity/user.entity';
 import { PhanLoai } from 'src/entity/speclist.entity';
 import { ChuyenNganh } from 'src/entity/spec.entity';
@@ -24,6 +25,10 @@ import { XetDuyetDeTai } from './entity/project-approval.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
     ScheduleModule.forRoot(),
     AuthModule,
     UserModule,
@@ -32,31 +37,35 @@ import { XetDuyetDeTai } from './entity/project-approval.entity';
     NotificationsModule,
     ProgressModule,
     DocumentsModule,
-    TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'localhost',
-      port: 1433,
-      username: 'hungmochi211',
-      password: 'Hung',
-      database: 'QuanLyDeTai',
-      entities: [
-        NguoiDung,
-        ChuyenNganh,
-        PhanLoai,
-        NguoiHD,
-        DeTai,
-        ThanhVienDT,
-        ThongBao,
-        MocDeTai,
-        TaiLieu,
-        ThanhVienMocDT,
-        XetDuyetDeTai,
-      ],
-      synchronize: false,
-      options: {
-        encrypt: false,
-        trustServerCertificate: true,
-      },
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mssql' as const,
+        host: config.getOrThrow<string>('DB_HOST'),
+        port: Number(config.get<string>('DB_PORT') ?? 1433),
+        username: config.getOrThrow<string>('DB_USERNAME'),
+        password: config.getOrThrow<string>('DB_PASSWORD'),
+        database: config.getOrThrow<string>('DB_DATABASE'),
+        entities: [
+          NguoiDung,
+          ChuyenNganh,
+          PhanLoai,
+          NguoiHD,
+          DeTai,
+          ThanhVienDT,
+          ThongBao,
+          MocDeTai,
+          TaiLieu,
+          ThanhVienMocDT,
+          XetDuyetDeTai,
+        ],
+        synchronize: config.get<string>('DB_SYNCHRONIZE') === 'true',
+        options: {
+          encrypt: config.get<string>('DB_ENCRYPT') === 'true',
+          trustServerCertificate:
+            config.get<string>('DB_TRUST_SERVER_CERTIFICATE') === 'true',
+        },
+      }),
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
   app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Cats example')
@@ -22,7 +24,7 @@ async function bootstrap() {
   });
 
   SwaggerModule.setup('api', app, documentFactoryApp);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(Number(configService.get<string>('PORT') ?? 3000));
 
   if (module.hot) {
     module.hot.accept();
